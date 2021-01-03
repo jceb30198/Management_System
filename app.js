8a35c0e8df0946233b88fc2354ff7d73b4bb7398
Jack Ceballos <jackrceballos@gmail.com>
2021-01-02 23:29:56 -0500

    Added dependencies.

---
+++ app.js
@@ -0,0 +1,13 @@
+// Dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+// MySQL connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "password",
+    database: "employee_managerDB"
+})
