035eb8d06bb64b447b39f72e23e6e4e047fa107b
Jack Ceballos <jackrceballos@gmail.com>
2021-01-23 19:51:12 -0500

    Add functions were created

--- app.js
+++ app.js
@@ -10,17 +10,158 @@ const connection = mysql.createConnection({
     user: "root",
     password: "password",
     database: "employee_managerDB"
-})
+});
 
 connection.connect((err)=> {
-    if (err) throw err;
-    console.log("Connected\n");
-})
+    if (err) {
+        throw err;
+    }
+    else {
+        console.log("Connected\n");
+        begin();
+    }
+});
 
-// Add department/role/employee
+ function begin() {
+    inquirer.prompt({
+        type: "list",
+        name: "task",
+        message: "What do you need help with?",
+        choices: ["Add Department", "Add Employee", "Add Role", "View Department(s)", "View Employee(s)", "View Role(s)", "Remove Department", "Remove Employee", "Remove Role", "Shutdown"]
+    }).then((res) => {
+        switch (res.task) {
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "View Department(s)":
 
-// View department/role/employee
+                
+                break;
+            case "View Employee(s)":
+                
+                break;
+            case "View Role(s)":
+                
+                break;
+            case "Remove Department":
+                
+                break;
+            case "Remove Employee":
+                
+                break;
+            case "Remove Role":
+                
+                break;
+            default:
+                console.log(`Goodbye\n--------------------------------------------------`);
+                connection.end();
+        };
+    })
+};
 
-// Remove department/role/employee
+function addDepartment() {
+    inquirer.prompt({
+        type: "input",
+        name: "newDepartment",
+        message: "What is the name of the new department?"
+    }).then((res) => {
+        let query = "INSERT INTO department SET ?";
+        connection.query(query, {name: res.newDepartment}, (err,res) => {
+            if (err) throw err;
+            console.log(`Department Added\n--------------------------------------------------`);
+            begin();
+        })
+    })
+};
 
-// Update roles of employees
+const addEmployeeArr = [
+    {
+        type: "input",
+        name: "firstName",
+        message: "Please enter the employee's first name:"
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "Please enter the employee's last name:"
+    },
+    {
+        type: "input",
+        name: "roleID",
+        message: "Please enter the employee's role ID:"
+    },
+    {
+        type: "input",
+        name: "managerID",
+        message: "Please enter the employee's manager ID:"
+    }
+];
+
+
+function addEmployee() {
+    inquirer.prompt(addEmployeeArr).then((res) => {
+        let query = "INSERT INTO employee SET ?";
+        connection.query(query, {
+            first_name: res.firstName,
+            last_name: res.lastName,
+            role_id: res.roleID,
+            manager_id: res.managerID
+        }, (err, res) => {
+            if (err) throw err;
+            console.log(`Employee Added\n--------------------------------------------------`)
+            begin();
+        })
+    })
+};
+
+const addRoleArr = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of the role?"
+    },
+    {
+        type: "input",
+        name: "salary",
+        message: "How much does this role make in salary?"
+    },
+    {
+        type: "input",
+        name: "departmentID",
+        message: "Please enter this role's department ID:"
+    }
+]
+
+function addRole() {
+    inquirer.prompt(addRoleArr).then((res) => {
+        let query = "INSERT INTO role SET ?";
+        connection.query(query, {
+            title: res.title,
+            salary: res.salary,
+            department_id: res.departmentID
+        }, (err, res) => {
+            if (err) throw err;
+            console.log(`Role Added\n--------------------------------------------------`);
+            begin();
+        })
+    })
+};
+
+/*
+What would you like to do? {
+    Add department/role/employee
+    
+    View department/role/employee
+    
+    Remove department/role/employee
+    
+    Update roles of employees
+}
+    
+    */
--- seed.sql
+++ seed.sql
@@ -4,23 +4,33 @@ CREATE DATABASE employee_managerDB;
 USE employee_managerDB;
 
 CREATE TABLE department (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
+INSERT INTO department (name)
+VALUES ("Management");
+
 CREATE TABLE role (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,2),
+    department_id INT,
     PRIMARY KEY (id)
 );
 
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 75000.00, 1);
+
 CREATE TABLE employee (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     PRIMARY KEY (id)
 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jack", "Ceballos", 1, 1)
